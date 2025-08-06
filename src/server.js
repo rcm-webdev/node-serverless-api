0e067fed0f277f13f3bbc0c252fa222128bdf575
aokiji <rcm.webdev.pro@gmail.com>
2025-08-06 16:16:55 -0700

    integrate nodejs to neon postgres and started to deploy to aws lambda via serverless framework

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const { neon, neonConfig } = require("@neondatabase/serverless");
+
 const app = express();
 
-app.get("/", (req, res, next) => {
+const dbClient = async () => {
+  //this is for http connections and non-pooling 
+  //no pooling in a serverless environment
+  neonConfig.fetchConnectionCache = true;
+  const sql = neon(process.env.DATABASE_URL);
+  return sql;
+}
+
+app.get("/", async (req, res, next) => {
+  const sql = await dbClient();
+  const results = await sql`SELECT now()`
   return res.status(200).json({
     message: "Hello from root!",
-    DATABASE_URL: process.env.DATABASE_URL,
+    results: results,
+    database_url: process.env.DATABASE_URL,
   });
 });
 
